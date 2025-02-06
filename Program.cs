b675f7f9e19e3703889eb016e89ac9ad51d8bdcf
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Air.local>
2025-02-06 09:14:14 -0700

    Finished Step: 2

--- Program.cs
+++ Program.cs
@@ -14,3 +14,22 @@ Console.WriteLine(c);
 
 c = a / b;
 Console.WriteLine(c);
+
+// Step 2
+a = 5;
+b = 4;
+c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
+
+d = (a + b) * c;
+Console.WriteLine(d);
+
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+a = 7;
+b = 4;
+c = 3;
+d = (a + b) / c;
+Console.WriteLine(d);
