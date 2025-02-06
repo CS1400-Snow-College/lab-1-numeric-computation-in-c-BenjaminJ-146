f578db874370161e5487481978349d21e5958edf
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Air.local>
2025-02-06 09:51:46 -0700

    Finished Step: 4

--- Program.cs
+++ Program.cs
@@ -49,3 +49,23 @@ Console.WriteLine($"The range of integers is {min} to {max}");
 
 int what = max + 3;
 Console.WriteLine($"An example of overflow: {what}");
+
+// Step 4
+double f = 5;
+double g = 4;
+double h = 2;
+double i = (f + g) / h;
+Console.WriteLine(i);
+
+f = 19;
+g = 23;
+h = 8;
+i = (f + g) / h;
+Console.WriteLine(i);
+
+double max2 = double.MaxValue;
+double min2 = double.MinValue;
+Console.WriteLine($"The range of doulbe is {min2} to {max2}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
